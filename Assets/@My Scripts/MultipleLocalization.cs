2a443deb8c6469542d216bccc5ee7b178d631d67
R4zielChicago <jcjc2k10@gmail.com>
2017-11-13 01:00:53 -0600

    Finally got multiple user inputs to display multiple markers on the map!! Optimized scripts to run using an InputManager script and Serializable class UserInput. Refined overall coding structure, to make things smoother at runtime.

--- Assets/@My Scripts/MultipleLocalization.cs
+++ Assets/@My Scripts/MultipleLocalization.cs
@@ -1,61 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
 
 // Analysis disable CheckNamespace
 public class MultipleLocalization : MonoBehaviour {
 // Analysis restore CheckNamespace
-	
 
-	// number of markers and their latitute and longitude
-	//public Vector2[] markerLatLon;
-	// this will be the colors of the markers
-	//public Color[] colors;
+
 	// this is the marker object with its own code and more:
 	public GameObject prefabMarker;
 	// this is the radii o the sphere/planet
 	public float R;
 
-	//public GameObject dataLoader;
-
 	public LoadCountryData countryData;
 
+	public InputManager inputManager;
 
 
+	void Start (){
 
-	void Start () 
-	{
-		
-//		LoadCountryData countryData = dataLoader.GetComponent<LoadCountryData> ();
-
-
-		//foreach (UserEntry user in XMLManager.ins.userDb.list) {
-
-
-			// for each connected player add the marker at the LATitude and LONgitude
-			//for (int jj = 0; jj < markerLatLon.Length; jj++) {
-				GameObject go = GameObject.Instantiate (prefabMarker, new Vector3 (0, 0, 0), Quaternion.Euler (0, 0, 0)) as GameObject;
-				go.transform.parent = transform;
-
-				// now we are setting the values on the marker script from the array of values
-				MarkerPositioning markScript = go.GetComponent<MarkerPositioning> ();
-
-				markScript.R = R;
-				//markScript.lat = markerLatLon [jj] [0];
-				markScript.lat = countryData.getLatitude ();
-			Debug.Log ("Latitude = " + markScript.lat);
-			//markScript.lon = markerLatLon [jj] [1];
-				markScript.lon = countryData.getLongitude ();
-			Debug.Log ("Longitude = " + markScript.lon);
-				//markScript.col = colors [jj];
-				markScript.col = countryData.getColor ();
-			//}
-
-
-		//}
+		InstantiateMarker ();
 	}
 
+	void InstantiateMarker(){
 
+		Debug.Log ("City Should be " + inputManager.inputs[0].userCity);
+		//foreach (UserInputs input in inputManager.inputs) {
 
+		for (int i = 0; i < inputManager.inputs.Count; i++) {
+			
+			// for each connected player add the marker at the LATitude and LONgitude
+			//for (int jj = 0; jj < markerLatLon.Length; jj++) {
+			GameObject go = GameObject.Instantiate (prefabMarker, new Vector3 (0, 0, 0), Quaternion.Euler (0, 0, 0)) as GameObject;
+			go.transform.parent = transform;
+
+			// now we are setting the values on the marker script from the array of values
+			MarkerPositioning markScript = go.GetComponent<MarkerPositioning> ();
+
+			markScript.R = R;
+			//markScript.lat = markerLatLon [jj] [0];
+			markScript.lat = countryData.getLatitude (inputManager.inputs[i]);
+			Debug.Log ("Latitude = " + markScript.lat);
+			//markScript.lon = markerLatLon [jj] [1];
+			markScript.lon = countryData.getLongitude (inputManager.inputs[i]);
+			Debug.Log ("Longitude = " + markScript.lon);
+			//markScript.col = colors [jj];
+			markScript.col = countryData.getColor (inputManager.inputs[i]);
+			//}
+		}
+	}
 }
