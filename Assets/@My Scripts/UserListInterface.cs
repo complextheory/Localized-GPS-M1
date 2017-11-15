ab14c658d3b91804a2e28362219c630d76a3890a
R4zielChicago <jcjc2k10@gmail.com>
2017-11-14 21:24:42 -0600

    Optimized code structure, to better compartmentalize components for quicker responses, and easier testing

---
+++ Assets/@My Scripts/UserListInterface.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserListInterface {
+
+	//public UserListManager ins;
+
+	 static List <UserInput>list;
+
+
+	public static void InitializeList ()
+	{
+		Debug.Log ("Should Initialize List");
+		list = new List<UserInput> ();
+
+
+		if (true) {
+
+			List<string> names = new List<string> {"Thomas", "Edward", "Henry", "Jason", "Penelope", 
+				"Kirt"
+			};
+			List<string> testCities = new List<string> {"Mobaye", "New York", "Dhaka", "Ravensthorpe",
+				"Yerevan", "Eisenstadt"
+			};
+			List<Color> colors = new List <Color> {Color.blue, Color.gray, Color.yellow, Color.blue,
+				Color.cyan, Color.red
+			};
+			//list = new List<UserInput> ();
+
+			for (int i = 0; i < testCities.Count; i++) {
+				UserInput tempUser = new UserInput ();
+				tempUser.userName = names[i];
+				tempUser.userCity = testCities[i];
+				tempUser.pictureName = "userPicture";
+				Debug.Log ("User Picture = " + tempUser.pictureName); 
+				tempUser.markerColor = colors[i];
+				list.Add (tempUser);
+			}
+			Debug.Log ("List = " + list.Count);
+		}else 
+			list = new List<UserInput>();
+	}
+			
+
+	public static void SetList(List<UserInput> loadedList){
+
+		list = loadedList;
+	}
+
+	public static void AddUser(UserInput user){
+
+		list.Add (user);
+	}
+
+	public static void RemoveUser(UserInput user){
+		list.Remove (user);
+	}
+
+	public static List<UserInput> GetList(){
+		return list;
+	}
+
+	public static int GetTotalUsers(){
+		return list.Count;
+	}
+
+
+	//Category 1 == Name 2 == Country. Reverse is whether to sort reverse or not
+	public static void SortList(int categoryNumber,bool reverse){
+
+		//sort by name alphabetecal order
+		if(categoryNumber == 1 && reverse == false){
+			
+			list.Sort((user1, user2) => user1.userName.CompareTo(user2.userName));
+		}
+
+		if(categoryNumber == 1 && reverse == true){
+			
+			list.Sort((user1, user2) => user2.userName.CompareTo(user1.userName));
+		}
+
+		if(categoryNumber == 2 && reverse == false){
+
+			list.Sort ((user1, user2) => user1.userCity.CompareTo (user2.userCity));
+		}
+
+		if(categoryNumber == 2 && reverse == true){
+
+			list.Sort ((user1, user2) => user2.userCity.CompareTo (user1.userCity));
+		}
+
+
+	}
+}
